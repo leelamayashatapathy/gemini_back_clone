ebddfe3b60c2fa9b0ea466f405ae8a657822668c
leelamayashatapathy <leelamayashatapathy@gmail.com>
2025-07-13 12:06:15 +0530

    Update Render config for Upstash Redis (rediss) and production Celery worker

--- gemini_backend/celery.py
+++ gemini_backend/celery.py
@@ -5,4 +5,20 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gemini_backend.settings')
 
 app = Celery('gemini_backend')
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Configure Celery for production with Upstash Redis
+app.conf.update(
+    broker_connection_retry_on_startup=True,
+    task_serializer='json',
+    accept_content=['json'],
+    result_serializer='json',
+    timezone='UTC',
+    enable_utc=True,
+    task_track_started=True,
+    task_time_limit=30 * 60,  # 30 minutes
+    task_soft_time_limit=25 * 60,  # 25 minutes
+    worker_prefetch_multiplier=1,
+    worker_max_tasks_per_child=1000,
+)
+
 app.autodiscover_tasks() 
